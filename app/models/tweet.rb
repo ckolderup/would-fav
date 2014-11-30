e056dd335444279b7b8d0e6fe06c8a5e21afc8f4
Casey Kolderup <casey@kolderup.org>
2014-11-30 14:17:07 -0500

    validate url exists

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,8 @@
 class Tweet < ActiveRecord::Base
   belongs_to :collection
+
+  validates_presence_of :url
+  validates_format_of :url, :with => URI::regexp(%w(http https))
+  #TODO: validate that the url is a twitter permalink
+
 end
